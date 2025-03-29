ac7c4204a662bd38c237a962ab691152c7e50bc2
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-03-29 14:48:41 +0700

    feat(payroll): add payroll service

--- src/services/payroll.service.ts
+++ src/services/payroll.service.ts
@@ -1,3 +1,67 @@
+import { Payroll } from "@prisma/client";
+import prisma from "../config/prisma.js";
+import { ResponseError } from "../error/response.errors.js";
+
 export class PayrollService {
-   
+   static async getAll():Promise<Payroll[]> {
+       const result = await prisma.payroll.findMany();
+       return result;
+   }
+
+   static async getById(id:number):Promise<Payroll> {
+    const result = await prisma.payroll.findUnique({
+        where: {
+            id
+        }
+    });
+    if(!result){
+        throw new Error("Payroll not found");
+    }
+    return result;
+   }
+
+   static async create(data:Payroll):Promise<Payroll> {
+    const result = await prisma.payroll.create({
+        data
+    });
+    return result;
+   }
+
+   static async update(id:number,data:Payroll):Promise<Payroll> {
+    const payrollExists = await prisma.payroll.findFirst({
+        where: {
+            id
+        }
+    });
+
+    if (!payrollExists) {
+        throw new ResponseError(404,"Data for payroll not found");
+    }
+
+    const result = await prisma.payroll.update({
+        where: {
+            id
+        },
+        data
+    });
+    return result;
+   }
+
+   static async delete(id:number):Promise<Payroll> {
+    const checkExistPayroll = await prisma.payroll.findFirst({
+        where: {
+            id
+        }
+    });
+
+    if (!checkExistPayroll) {
+        throw new ResponseError(404,"Data for payroll not found");
+    }
+    const result = await prisma.payroll.delete({
+        where: {
+            id
+        }
+    });
+    return result;
+   }
 }
