e1168564b22f3239033227e63cb25cec41b536a6
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-04-05 20:51:24 +0700

    feat(transfer-request): add transfer request unit test

--- src/services/transferRequest.service.ts
+++ src/services/transferRequest.service.ts
@@ -1,197 +1,224 @@
-import { TransferRequest, TransferRequestStatus } from "@prisma/client";
-import prisma from "../config/prisma.js";
-import { ResponseError } from "../error/response.errors.js";
-import { TransferRequestForEmployee, TransferRequestForHrd } from "../types/transferRequest.type.js";
-import { Validation } from "../validations/validation.js";
-import { transferRequestForEmployeeSchema, transferRequestForHRDSchema } from "../validations/transferRequest.validation.js";
-
+import { TransferRequest, TransferRequestStatus } from '@prisma/client';
+import prisma from '../config/prisma.js';
+import { ResponseError } from '../error/response.errors.js';
+import {
+    TransferRequestForEmployee,
+    TransferRequestForHrd,
+} from '../types/transferRequest.type.js';
+import { Validation } from '../validations/validation.js';
+import {
+    transferRequestForEmployeeSchema,
+    transferRequestForHRDSchema,
+} from '../validations/transferRequest.validation.js';
 
 export class TransferRequestService {
-    static async getAll():Promise<TransferRequest[]> {
+    static async getAll(): Promise<TransferRequest[]> {
         const result = await prisma.transferRequest.findMany();
         return result;
     }
 
-    static async getById(id:number):Promise<TransferRequest> {
+    static async getById(id: number): Promise<TransferRequest> {
         const result = await prisma.transferRequest.findUnique({
             where: {
-                id
-            }
-        });
-
-        if(!result){
-            throw new ResponseError( 404,"Transfer Request not found");
-        }
-        return result;
-    }
-
-    static async createFromEmployee(data:TransferRequestForEmployee):Promise<TransferRequest> {
-        const validate = Validation.validate<TransferRequestForEmployee>(transferRequestForEmployeeSchema,data);
-        const count = await prisma.transferRequest.count({
-            where: {
-                employeeId: validate.employeeId,
-                fromOffice: validate.fromOffice,
-                toOffice: validate.toOffice
-            }
-        });
-
-        if (count > 0) {
-            throw new ResponseError(409,"Data for transfer request already exists");
-        }
-
-       return await prisma.transferRequest.create({
-            data
-        });
-      
-    }
-    static async updateFromEmployee(id:number,data:TransferRequestForEmployee):Promise<TransferRequest> {
-        const validate = Validation.validate<TransferRequestForEmployee>(transferRequestForEmployeeSchema,data);
-        const checkExistRequest = await prisma.transferRequest.findFirst({
-            where: {
-                id
-            }
-        }); 
-
-        if (!checkExistRequest) {
-            throw new ResponseError(404,"Data for transfer request not found");
-        }
-        if (checkExistRequest.status !== TransferRequestStatus.PENDING) {
-            throw new ResponseError(409,"Transfer request cannot be updated because it is not pending");
-        }
-        const result = await prisma.transferRequest.update({
-            where: {
-                id
+                id,
             },
-            data: {
-                employeeId: validate.employeeId,
-                fromOffice: validate.fromOffice,
-                toOffice: validate.toOffice
-            }    
         });
 
+        if (!result) {
+            throw new ResponseError(404, 'Transfer Request not found');
+        }
         return result;
     }
-   
 
-    static async createFromHRD(data:TransferRequestForHrd):Promise<TransferRequest> {
-        const validate = Validation.validate<TransferRequestForHrd>(transferRequestForHRDSchema,data);
+    static async createFromEmployee(data: TransferRequestForEmployee): Promise<TransferRequest> {
+        const validate = Validation.validate<TransferRequestForEmployee>(
+            transferRequestForEmployeeSchema,
+            data,
+        );
         const count = await prisma.transferRequest.count({
             where: {
                 employeeId: validate.employeeId,
                 fromOffice: validate.fromOffice,
                 toOffice: validate.toOffice,
-                approvedBy: validate.approvedBy
-            }
-        }); 
+            },
+        });
 
         if (count > 0) {
-            throw new ResponseError(409,"Data for transfer request already exists");
-        }   
+            throw new ResponseError(409, 'Data for transfer request already exists');
+        }
 
         return await prisma.transferRequest.create({
-            data
+            data,
         });
     }
-    static async updateFromHRD(id:number,data:TransferRequestForHrd):Promise<TransferRequest> {
-        const validate = Validation.validate<TransferRequestForHrd>(transferRequestForHRDSchema,data);
+    static async updateFromEmployee(
+        id: number,
+        data: TransferRequestForEmployee,
+    ): Promise<TransferRequest> {
+        const validate = Validation.validate<TransferRequestForEmployee>(
+            transferRequestForEmployeeSchema,
+            data,
+        );
         const checkExistRequest = await prisma.transferRequest.findFirst({
             where: {
-                id
-            }
+                id,
+            },
         });
 
         if (!checkExistRequest) {
-            throw new ResponseError(404,"Data for transfer request not found");
+            throw new ResponseError(404, 'Data for transfer request not found');
+        }
+        if (checkExistRequest.status !== TransferRequestStatus.PENDING) {
+            throw new ResponseError(
+                409,
+                'Transfer request cannot be updated because it is not pending',
+            );
         }
         const result = await prisma.transferRequest.update({
             where: {
-                id    
+                id,
             },
             data: {
                 employeeId: validate.employeeId,
                 fromOffice: validate.fromOffice,
                 toOffice: validate.toOffice,
-                approvedBy: validate.approvedBy
-            }
+            },
         });
 
         return result;
     }
 
-    static async delete(id:number):Promise<TransferRequest> {
+    static async createFromHRD(data: TransferRequestForHrd): Promise<TransferRequest> {
+        const validate = Validation.validate<TransferRequestForHrd>(
+            transferRequestForHRDSchema,
+            data,
+        );
+        const count = await prisma.transferRequest.count({
+            where: {
+                employeeId: validate.employeeId,
+                fromOffice: validate.fromOffice,
+                toOffice: validate.toOffice,
+                approvedBy: validate.approvedBy,
+            },
+        });
+
+        if (count > 0) {
+            throw new ResponseError(409, 'Data for transfer request already exists');
+        }
+
+        return await prisma.transferRequest.create({
+            data,
+        });
+    }
+    static async updateFromHRD(id: number, data: TransferRequestForHrd): Promise<TransferRequest> {
+        const validate = Validation.validate<TransferRequestForHrd>(
+            transferRequestForHRDSchema,
+            data,
+        );
         const checkExistRequest = await prisma.transferRequest.findFirst({
             where: {
-                id
-            }
+                id,
+            },
+        });
+
+        if (!checkExistRequest) {
+            throw new ResponseError(404, 'Data for transfer request not found');
+        }
+        const result = await prisma.transferRequest.update({
+            where: {
+                id,
+            },
+            data: {
+                employeeId: validate.employeeId,
+                fromOffice: validate.fromOffice,
+                toOffice: validate.toOffice,
+                approvedBy: validate.approvedBy,
+            },
+        });
+
+        return result;
+    }
+
+    static async delete(id: number): Promise<TransferRequest> {
+        const checkExistRequest = await prisma.transferRequest.findFirst({
+            where: {
+                id,
+            },
         });
         if (!checkExistRequest) {
-            throw new ResponseError(404,"Data for transfer request not found");
+            throw new ResponseError(404, 'Data for transfer request not found');
         }
         if (checkExistRequest.status !== TransferRequestStatus.PENDING) {
-            throw new ResponseError(409,"Transfer request cannot be deleted because it is not pending");
+            throw new ResponseError(
+                409,
+                'Transfer request cannot be deleted because it is not pending',
+            );
         }
 
         const result = await prisma.transferRequest.delete({
             where: {
-                id
-            }
+                id,
+            },
         });
         return result;
     }
-    
-    static async approve(id:number):Promise<TransferRequest> {
-        const checkExistRequest = await prisma.transferRequest.findFirst({
+
+    static async approve(id: number): Promise<TransferRequest> {
+        const checkExistRequest = await prisma.transferRequest.findUnique({
             where: {
-                id
-            }
+                id,
+            },
         });
 
         if (!checkExistRequest) {
-            throw new ResponseError(404,"Data for transfer request not found");
+            throw new ResponseError(404, 'Data for transfer request not found');
         }
 
         if (checkExistRequest.status !== TransferRequestStatus.PENDING) {
-            throw new ResponseError(409,"Transfer request cannot be approved because it is not pending");
+            throw new ResponseError(
+                409,
+                'Transfer request cannot be approved because it is not pending',
+            );
         }
 
         const result = await prisma.transferRequest.update({
             where: {
-                id
+                id,
             },
             data: {
-                status: TransferRequestStatus.APPROVED
-            }
+                status: TransferRequestStatus.APPROVED,
+            },
         });
-        
+
         return result;
-      
     }
 
-    static async reject(id:number):Promise<TransferRequest> {
+    static async reject(id: number): Promise<TransferRequest> {
         const checkExistRequest = await prisma.transferRequest.findFirst({
             where: {
-                id
-            }
+                id,
+            },
         });
 
         if (!checkExistRequest) {
-            throw new ResponseError(404,"Data for transfer request not found");
+            throw new ResponseError(404, 'Data for transfer request not found');
         }
 
         if (checkExistRequest.status !== TransferRequestStatus.PENDING) {
-            throw new ResponseError(409,"Transfer request cannot be rejected because it is not pending");
+            throw new ResponseError(
+                409,
+                'Transfer request cannot be rejected because it is not pending',
+            );
         }
 
         const result = await prisma.transferRequest.update({
             where: {
-                id
+                id,
             },
             data: {
-                status: TransferRequestStatus.REJECTED
-            }
+                status: TransferRequestStatus.REJECTED,
+            },
         });
         return result;
     }
-
-
 }
---
+++ test/transferRequest.test.ts
@@ -0,0 +1,306 @@
+import request from 'supertest';
+import app from '../src/app';
+import prisma from '../src/config/prisma';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { mockedToken, loginAndGetToken } from './utils/authHelper';
+import { TransferRequestStatus } from '@prisma/client';
+
+jest.mocked(prisma);
+jest.mocked('bcrypt');
+jest.mocked('jsonwebtoken');
+
+describe('Transfer Request Service', () => {
+    let token: string;
+    beforeAll(async () => {
+        token = await mockedToken();
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    it('should return 401 if the user is not authenticated', async () => {
+        const response = await request(app)
+            .get('/api/transfer-request')
+            .set('Authorization', `Invalid token`)
+            .send({});
+        expect(response.status).toBe(401);
+        expect(response.body.message).toBe('Unauthorized: Token is missing or invalid');
+    });
+
+    it('should return 200 fetching all transfer requests', async () => {
+        const mockdata = [
+            {
+                id: 1,
+                employeeId: 1,
+                officeId: 1,
+            },
+        ];
+        prisma.transferRequest.findMany = jest.fn().mockResolvedValue(mockdata);
+        const response = await request(app)
+            .get('/api/transfer-request')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+        expect(response.status).toBe(200);
+        expect(response.body.data).toEqual(mockdata);
+    });
+
+    it('should return 200 fetching transfer request by id', async () => {
+        prisma.transferRequest.findUnique = jest.fn().mockResolvedValue({
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        const response = await request(app)
+            .get('/api/transfer-request/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+        expect(response.status).toBe(200);
+        expect(response.body.data).toEqual({
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+    });
+
+    it('should return 404 if the transfer request is not found', async () => {
+        prisma.transferRequest.findUnique = jest.fn().mockResolvedValue(null);
+        const response = await request(app)
+            .get('/api/transfer-request/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe('Transfer Request not found');
+    });
+
+    it('should return 201 creating transfer request successfully from employee', async () => {
+        prisma.transferRequest.count = jest.fn().mockResolvedValue(0);
+        prisma.transferRequest.create = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        const response = await request(app)
+            .post('/api/transfer-request/create-from-employee')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                status: TransferRequestStatus.PENDING,
+            });
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Transfer request created successfully');
+        expect(response.body.data).toEqual({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+    });
+    it('should return 409 if the transfer request already exists from employee', async () => {
+        prisma.transferRequest.count = jest.fn().mockResolvedValue(1);
+        const response = await request(app)
+            .post('/api/transfer-request/create-from-employee')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                status: TransferRequestStatus.PENDING,
+            });
+        expect(response.status).toBe(409);
+        expect(response.body.message).toBe('Data for transfer request already exists');
+    });
+
+    it('should return 201 creating transfer request successfully from hrd', async () => {
+        prisma.transferRequest.count = jest.fn().mockResolvedValue(0);
+        prisma.transferRequest.create = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        const response = await request(app)
+            .post('/api/transfer-request/create-from-hrd')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                approvedBy: 1,
+                status: TransferRequestStatus.PENDING,
+            });
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Transfer request created successfully');
+        expect(response.body.data).toEqual({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+    });
+    it('should return 409 if the transfer request already exists from hrd', async () => {
+        prisma.transferRequest.count = jest.fn().mockResolvedValue(1);
+        const response = await request(app)
+            .post('/api/transfer-request/create-from-hrd')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                approvedBy: 1,
+                status: TransferRequestStatus.PENDING,
+            });
+        expect(response.status).toBe(409);
+        expect(response.body.message).toBe('Data for transfer request already exists');
+    });
+    it('should return 200 updating transfer request successfully from employee', async () => {
+        prisma.transferRequest.findFirst = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        prisma.transferRequest.update = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.APPROVED,
+        });
+        const response = await request(app)
+            .put('/api/transfer-request/update-from-employee/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                status: TransferRequestStatus.APPROVED,
+            });
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Transfer request updated successfully');
+        expect(response.body.data).toEqual({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.APPROVED,
+        });
+    });
+    it('should return 409 if the transfer request status is not pending from employee', async () => {
+        prisma.transferRequest.findFirst = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            status: TransferRequestStatus.APPROVED,
+        });
+        const response = await request(app)
+            .put('/api/transfer-request/update-from-employee/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                status: TransferRequestStatus.APPROVED,
+            });
+        expect(response.status).toBe(409);
+        expect(response.body.message).toBe(
+            'Transfer request cannot be updated because it is not pending',
+        );
+    });
+
+    it('should return 200 updating transfer request successfully from hrd', async () => {
+        prisma.transferRequest.findFirst = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        prisma.transferRequest.update = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.APPROVED,
+        });
+        const response = await request(app)
+            .put('/api/transfer-request/update-from-hrd/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                employeeId: 1,
+                fromOffice: 1,
+                toOffice: 1,
+                approvedBy: 1,
+                status: TransferRequestStatus.APPROVED,
+            });
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Transfer request updated successfully');
+        expect(response.body.data).toEqual({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.APPROVED,
+        });
+    });
+    it('should return 200 approve transfer request successfully', async () => {
+        prisma.transferRequest.findUnique = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        prisma.transferRequest.update = jest.fn().mockResolvedValue({
+            status: TransferRequestStatus.APPROVED,
+        });
+        const response = await request(app)
+            .patch('/api/transfer-request/approve/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Transfer request approved successfully');
+        expect(response.body.data).toEqual({
+            status: 'APPROVED',
+        });
+    });
+    it('should return 200 reject transfer request successfully', async () => {
+        prisma.transferRequest.findUnique = jest.fn().mockResolvedValue({
+            id: 1,
+            employeeId: 1,
+            fromOfficeId: 1,
+            toOfficeId: 1,
+            approvedBy: 1,
+            status: TransferRequestStatus.PENDING,
+        });
+        prisma.transferRequest.update = jest.fn().mockResolvedValue({
+            status: TransferRequestStatus.REJECTED,
+        });
+        const response = await request(app)
+            .patch('/api/transfer-request/reject/1')
+            .set('Authorization', `Bearer ${token}`)
+            .send();
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('Transfer request rejected successfully');
+        expect(response.body.data).toEqual({
+            status: 'REJECTED',
+        });
+    });
+});
